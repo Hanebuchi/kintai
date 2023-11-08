287834d49387ee10f56389c7d200b590afc33931
Hanebuchi <137970413+Hanebuchi@users.noreply.github.com>
2023-11-08 16:29:13 +0900

    Add files via upload

---
+++ AttandanceManege/src/main/java/com/example/AttandanceManege/AttandanceManegeApplication.java
@@ -0,0 +1,13 @@
+package com.example.AttandanceManege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttandanceManegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttandanceManegeApplication.class, args);
+	}
+
+}
---
+++ AttandanceManege/src/test/java/com/example/AttandanceManege/AttandanceManegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AttandanceManege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttandanceManegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
