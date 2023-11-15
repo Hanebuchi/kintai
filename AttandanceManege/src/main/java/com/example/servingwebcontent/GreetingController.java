1d70db4fa76adeb0406ed402b7e3eac02f1c0d6c
Hanebuchi <137970413+Hanebuchi@users.noreply.github.com>
2023-11-15 11:07:40 +0900

    Add files via upload

--- AttandanceManege/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ AttandanceManege/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,18 +1,34 @@
 package com.example.servingwebcontent;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class GreetingController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
+    @GetMapping("/test")
+    public String greeting(Model model) {
+        String sql = "SELECT * FROM ATTENDANCES";
+
+        List<Map<String,Object>> list = jdbcTemplate.queryForList(sql);
+        model.addAttribute("test",list);
+        return "test";
+    }
+
 
     @GetMapping("/login")
     public String login(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
